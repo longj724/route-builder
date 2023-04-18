d54ff547b58c74f0609c64a2358422ea24be0bc8
juslong <juslong@umich.edu>
2023-04-18 12:27:04 -0600

    Link to Google Maps from markers

--- frontend/src/Components/Points.tsx
+++ frontend/src/Components/Points.tsx
@@ -1,6 +1,8 @@
 // External Dependencies
-import { ReactElement } from 'react';
+import React, { ReactElement, useRef } from 'react';
 import { Marker } from 'react-map-gl';
+import { Box, Button } from '@chakra-ui/react';
+import { FaMapMarkerAlt } from 'react-icons/fa';
 
 // Relative Dependencies
 import { useRoute } from '../Context/RouteProvider';
@@ -11,8 +13,20 @@ export type Point = {
   lng: number;
 };
 
+interface Refs {
+  [key: number]: HTMLButtonElement;
+}
+
 function Points(): ReactElement {
   const { selectedPoints } = useRoute().route;
+
+  const markerRefs = useRef<Refs | null>({});
+
+  const handleMarkerRefs = (index: number) => (ref: HTMLButtonElement) => {
+    if (markerRefs.current) {
+      markerRefs.current[index] = ref;
+    }
+  };
 
   const getMarkerColor = (index: number) => {
     if (index === 0) {
@@ -22,16 +36,61 @@ function Points(): ReactElement {
     }
   };
 
+  const displayGoogleMapsButton = (buttonIndex: number) => {
+    if (markerRefs.current) {
+      markerRefs.current[buttonIndex].style.display = 'initial';
+    }
+  };
+
+  const hideGoogleMapsButton = (buttonIndex: number) => {
+    if (markerRefs.current) {
+      markerRefs.current[buttonIndex].style.display = 'none';
+    }
+  };
+
+  const handleGoogleMapButtonClick = (
+    event: React.MouseEvent<HTMLButtonElement>,
+    lat: number,
+    lng: number
+  ) => {
+    event.stopPropagation();
+    window.open(
+      `https://www.google.com/maps/search/?api=1&query=${lat},${lng}`,
+      '_blank'
+    );
+  };
+
   return (
     <>
       {selectedPoints.map(({ lat, lng }, index) => (
         <Marker
-          color={getMarkerColor(index)}
           latitude={lat}
           longitude={lng}
-          anchor="center"
-          key={lat.toString() + lng.toString()}
-        />
+          anchor="bottom"
+          key={`${lat}+${lng}`}
+        >
+          <Box
+            width="100px"
+            height="100px"
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="end"
+            gap={2}
+            onMouseEnter={() => displayGoogleMapsButton(index)}
+            onMouseLeave={() => hideGoogleMapsButton(index)}
+          >
+            <Button
+              onClick={(event) => handleGoogleMapButtonClick(event, lat, lng)}
+              colorScheme="blue"
+              ref={handleMarkerRefs(index)}
+              display="none"
+            >
+              View in Google Maps
+            </Button>
+            <FaMapMarkerAlt size={36} color={getMarkerColor(index)} />
+          </Box>
+        </Marker>
       ))}
     </>
   );
